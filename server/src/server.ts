215e91626212789190cf481dff34c607db706155
Pahulveer <pahulveersinghsohal@gmail.com>
2026-09-26 04:50:01 +0530

    Serve Vault frontend from Express

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import { nodeManager } from './nodes/NodeManager';
 import { metadataManager } from './metadata/MetadataManager';
 import { apiRouter } from './routes/api';
@@ -12,6 +13,17 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
+const clientDist = path.resolve(__dirname, '../../client/dist');
+
+app.use(express.static(clientDist));
+
+app.use((req, res, next) => {
+  if (req.method === 'GET' && !req.path.startsWith('/api')) {
+    res.sendFile(path.join(clientDist, 'index.html'));
+    return;
+  }
+  next();
+});
 
 async function bootstrap() {
   try {
